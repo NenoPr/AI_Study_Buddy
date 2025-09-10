d4fbd0a8042369426c5cef31270dd33ca7c0172f
NenoPr <nenadpredan@gmail.com>
2025-09-10 12:47:03 +0200

    Added cors to frontend site

--- server/server.mjs
+++ server/server.mjs
@@ -18,8 +18,7 @@ if (!process.env.JWT_SECRET) {
 
 const app = express();
 app.use(cookieParser());
-
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ai-study-buddy-23z2.onrender.com", credentials: true }));
 app.use(express.json());
 app.use((req, res, next) => {
     req.pool = pool;
