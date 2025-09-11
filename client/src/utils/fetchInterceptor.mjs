b0ace4a6de16697d2daec779387aee5e3eebfbf5
NenoPr <nenadpredan@gmail.com>
2025-09-11 12:06:50 +0200

    Added on the fetch interceptor to make sure the url is properly formated.

--- client/src/utils/fetchInterceptor.mjs
+++ client/src/utils/fetchInterceptor.mjs
@@ -1,10 +1,27 @@
 // fetchInterceptor.js
 import { navigate } from "./navigate";
+// utils/fetchInterceptor.js
+const API_BASE = import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "";
+
+function normalizeUrl(input) {
+  // If full absolute URL (http/https), return as is
+  if (/^https?:\/\//i.test(input)) return input;
+
+  // Otherwise, prepend API_BASE
+  return `${API_BASE}${input.startsWith("/") ? input : "/" + input}`;
+}
 
 const originalFetch = window.fetch;
 let isRedirecting = false;
 
 window.fetch = async (...args) => {
+  let url = args[0];
+  if (typeof url === "string") {
+    url = normalizeUrl(url);
+    args[0] = url;
+  }
+  url = normalizeUrl(url);
+
   const res = await originalFetch(...args);
 
   if (res.status === 401 && !isRedirecting) {
