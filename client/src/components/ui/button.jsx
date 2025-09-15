fdb97508e6c53b6a808b76d1e056c1fd90672178
NenoPr <nenadpredan@gmail.com>
2025-09-15 12:25:36 +0200

    Updated button and index.css to change button color again...

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-gray-500 text-black shadow hover:bg-gray-800",
+          "bg-gray-500 text-black shadow hover:bg-gray-800 dark:bg-gray-500",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
